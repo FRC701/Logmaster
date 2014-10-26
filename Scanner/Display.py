f4a93778ed4fbd9fc3fb3a19f323c427e4bed885
Andrew Pirondini <andrewjpiro@gmail.com>
2014-10-26 13:25:02 -0700

    Cleaning and commenting!

--- Scanner/Display.py
+++ Scanner/Display.py
@@ -3,32 +3,30 @@ from Hours import getCurrentHours
 import sqlite3
 import subprocess
 
-execString = "../../BeagleDisplay1"
+execString = "../BeagleDisplay1"
+
+#Executes display program as a subprocess using its stdin and stdout as pipes
 p = subprocess.Popen([execString,"-qws"],
                       stdin=subprocess.PIPE,
                       stdout=subprocess.PIPE)
-
+                      
+#Sends formatted string to display file
 def sendMessage(message):
 	childIn = p.stdin
 	childIn.flush()
 	childIn.write(message)
 
+#Creates a formatted string containing all the info to be displayed.
+#Calls sendMessage on this formatted string.
 def display(string, hours, name, database):
 	conn = sqlite3.connect(database)
 	output = string + " " + name + " "
 	if name:
 	    if hours:
-	        print "Goodbye " + name
 		output += "Out "
-	        print "Earned Hours: "
-	        print hours
-		output +=  str(hours) +" "
+		output +=  str(hours) + " "
 	    else:
-	        print "Welcome " + name
 		output += "In 0 "
-	    print "Total Hours: "
-	    print getCurrentHours(string, conn)
-	    print datetime.datetime.now()
 	    sendMessage(output)	    	
 	else:
 	    print "Unknown tag. Please enter into database first"
