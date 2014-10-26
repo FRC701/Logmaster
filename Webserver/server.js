df3cfa82aa4a743f91659b1cb13d9fa82e75081b
Andrew Pirondini <andrewjpiro@gmail.com>
2014-10-26 12:42:09 -0700

    Cleaning and conmenting!

--- Webserver/server.js
+++ Webserver/server.js
@@ -1,22 +1,33 @@
+/*Server for the Logmaster RFID Scanner using a BeagleBone Black.
+Uses express to handle the server requests and responds with data from
+the sqlite database created in the python program for the Logmaster. 
+Runs as a daemon on start up and currently must be in the directory
+"/home/root/Logmaster/Webserver" to function properly. */
+
+//Importing required node modules
 var fs = require("fs");
 var child_process = require('child_process');
 var http = require("http");
 var express = require("express");
 var sqlite3 = require("sqlite3");
-//var daemon = require('daemon')();
+var daemon = require('daemon')();
+var bodyParser = require('body-parser');
+
+//Setting up global database and port
 var repository = "/home/root/Logmaster/Database-2013.db"
 var exists = fs.existsSync(repository);
 var db = new sqlite3.Database(repository);
-var bodyParser = require('body-parser');
 
 var port = 8080;
 		  
-listen(port);
+listen(port); //main process for this server
+
 function listen(port)
 {
 	var app = express();
 	app.use(bodyParser());
-
+        
+        //Links requests to their corresponding handler functions
 	app.get('/', getClassLog);
 	app.get('/class/:name',getClass);
 	app.get('/class', getClass);
@@ -25,64 +36,82 @@ function listen(port)
 	app.get('/hours/:name',getHours);
 	app.get('/hours', getHours);
 	app.post('/input', handleInput);
+	
 	var server = http.createServer(app);
 	server.listen(port);
 }
+
 function handleInput(req, res)
 {
-    console.log("handleInput");
+    //TODO: send over password in a more secure
+    //format and use as an admin login
     res.send("You sent the name " + req.body.username);
 }
 
 function getClassLog(req, res)
 {
-	sql = "SELECT * FROM classLog";
+	sql = "SELECT * FROM classLog"; //Default view
 	sendData(res, "Tag,Name,hours", sql);
 }
+
 function getClass(req, res)
 {
 	var name = req.params.name;
 	sql = "SELECT Tag,Name FROM class";
 	if(name)
 		sql += " WHERE name = ?"
-	console.log(sql);
 	sendData(res,"Tag,Name", sql, name);
 }
+
 function getHours(req, res)
 {
  	var name = req.params.name;
 	sql = "SELECT Name,hours FROM classLog";
 	if(name)
 		sql += " WHERE name = ?"
-	console.log(sql);
 	sendData(res, "Name,hours", sql, name);
 }
+
 function getLog(req, res)
 { 
     var tag = req.params.tag;
     sql = "SELECT tag,time FROM log";
     if(tag)
 	sql += " WHERE tag = ?"
-    console.log(sql);	
     sendData(res, "tag,time", sql, tag);
 }
-	
+
+/*Queries the database using the given sql and params
+and stores the data in a two dimensional array. This array
+is then passed into sendTable for formatting.*/
+
+//columns: a constant string expression with column titles separated by ','
 function sendData(res, columns, sql, params)
 {
 	var splitColumns = columns.split(",");
 	var nColumns = splitColumns.length;
 	var storedData = new Array(nColumns);
+	
+	//creates two dimensional array.
 	for(i = 0; i<nColumns; i++)
 	{
 		storedData[i] = new Array();
 	}
+	
+	//makes column title the first element in each column.
 	for(i in splitColumns)
 	{
 		storedData[i][0] = splitColumns[i];
 	}
+	
+	//Fills array with data from sqlite database.
 	db.serialize(function()
 	{
-	   var location = 1;
+	   var location = 1; //Second iterator for second dimension of array.
+	   
+	   /*sqlite3 command with two callback functions:
+	   First one performs its operation on each row.
+	   Second one executes once after all row are done being processed.*/
 	   db.each(sql,params,
 	   function(err,row)
 	   {
@@ -95,16 +124,22 @@ function sendData(res, columns, sql, params)
 	    },
 	    function(err,rows)
 	    {
-	      console.log(nColumns); 
 	      sendTable(rows, nColumns, storedData, res);
 	    });
     });
 }
+/*Sends two dimensional array passed in as 'data' with
+number of rows 'rows' and number of columns 'nColums'
+to 'res' in the format of an html table*/
 
+//TODO: Replace with angular and json so it 
+//will be so much cleaner :)
 	
 function sendTable(rows, nColumns, data, res)
 {
-   var responseText = '<html><body><table border = "1"><tr><td>';
+   //responseText holds html table as it gets created from the array.
+   var responseText = '<table border = "1"><tr><td>';
+   
    for(i = 0; i<=rows; i++)
    {
      for(i2 = 0; i2<nColumns; i2++)
@@ -119,9 +154,12 @@ function sendTable(rows, nColumns, data, res)
     else
        responseText += '</tr>';
     }
-    responseText += "</table></body></html>";
+    responseText += "</table>";
+    
     fs.readFile('/home/root/Logmaster/Webserver/pageLayout.html', 
 		'utf8', sendFile);
+		
+    //Injects table into layout file where appropriate.		
     function sendFile(err, file)
     {
 	file = file.replace(/_INSERT_TABLE_/g, responseText);
