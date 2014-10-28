1a470992e8d6047a1d50e5eaabe0eac34606b40f
Andrew Pirondini <andrewjpiro@gmail.com>
2014-10-27 21:41:42 -0700

    Commenting and cleaning!

--- Scanner/Scanner.py
+++ Scanner/Scanner.py
@@ -1,26 +1,32 @@
 import serial
 import time
 import sys
-from Log import log
-from RFID import *
-from Display import display
+
 from threading import *
-from LoggedIn import update_logged_in
-from Hours import calculateHours
+
+from RFID import *
 from Name import lookUpName
+from Log import log
+from LoggedIn import update_logged_in
+from Display import display
 
 serial = serial.Serial("/dev/ttyUSB0", baudrate = 2400)
 database = "../Database-2013.db"
+
+#Scan is the main process for the RFID Scanner. 
+#It is run every two seconds after it exits through the Timer t.
+#It puts together the code from all other imported files. 
+
 def scan():
-    rfidTag = rfidRead(serial)
-    name = lookUpName(rfidTag, database)
+    rfidTag = rfidRead(serial) #From RFID.py returns tag scanned as a string
+    name = lookUpName(rfidTag, database) #Returns 0 if no match in database.
     if name:
-        log(rfidTag, database)
-        hours = update_logged_in(rfidTag, database)
-        display(rfidTag, hours, name, database)
+        log(rfidTag, database) #Simply adds log file with time and tag to database.
+        hours = update_logged_in(rfidTag, database)#Logs in/out person and updates hours if needed.
+        display(rfidTag, hours, name, database)#Runs BeagleDisplay1 as subprocess and pipes over data.
     else:
 	print "Unknown Tag or Noise"
 while True:
-    if activeCount() < 2:
-	t = Timer(2, scan)
+    if activeCount() < 2:     #The display subprocess makes this 2 when scan is running and 1 when scan exits.
+	t = Timer(2, scan)    #Run every two seconds. Gives person time to remove tag before double read.
 	t.start()
